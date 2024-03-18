f7af6f32de14dbf6f166a7f9aecda553f0d79078
sanoop <sanoopse@gmail.com>
2024-03-18 09:29:47 +0530

    Quotation mark remove in row names

--- ETL_Project1/csvextract.py
+++ ETL_Project1/csvextract.py
@@ -11,16 +11,17 @@ df=pd.read_csv(r'D:\SanoopDE\customers-1000.csv')
 # Define the table schema
 create_table_query = """
 CREATE TABLE Customers (
-    "customer id" VARCHAR(255),
-    "first name" VARCHAR(255),
-    "last name" VARCHAR(255),
+    Index INT,
+    customer_id VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
     company VARCHAR(255),
     city VARCHAR(255),
     country VARCHAR(255),
-    "phone 1" VARCHAR(255),
-    "phone 2" VARCHAR(255),
+    phone_1 VARCHAR(255),
+    phone_2 VARCHAR(255),
     email VARCHAR(255),
-    "subscription date" VARCHAR(255),
+    subscription_date VARCHAR(255),
     website VARCHAR(255)
 )
 """
@@ -37,8 +38,8 @@ cursor.execute(create_table_query)
 # Insert DataFrame records into the PostgreSQL database
 for index, row in df.iterrows():
     cursor.execute(
-        'INSERT INTO Customers ("customer id", "first name", "last name", company, city, country, "phone 1", "phone 2", email, "subscription date", website) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
-        (row['Customer Id'], row['First Name'], row['Last Name'], row['Company'], row['City'], row['Country'], row['Phone 1'], row['Phone 2'], row['Email'], row['Subscription Date'], row['Website'])
+        'INSERT INTO Customers (Index,customer_id, first_name, last_name, company, city, country, phone_1, phone_2, email, subscription_date, website) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+        (row['Index'],row['Customer Id'], row['First Name'], row['Last Name'], row['Company'], row['City'], row['Country'], row['Phone 1'], row['Phone 2'], row['Email'], row['Subscription Date'], row['Website'])
     )
 
 # Commit the transaction
